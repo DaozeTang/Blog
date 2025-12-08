7f4be479c1556b76dc2470e83ac5668dec4b2473
DaozeTang <2420957382@qq.com>
2025-12-08 23:04:18 +0800

    up

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,11 +2,11 @@ import type { ProfileConfig } from "../types/config";
 
 export const profileConfig: ProfileConfig = {
 	// 头像
-	avatar: "/assets/images/avatar.webp",
+	avatar: "/assets/images/avatar.jpg",
 	// 名字
-	name: "Firefly",
+	name: "Daoze Tang",
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "Hello, I'm Daoze Tang.",
 
 	// 链接配置
 	// 已经预装的图标集：fa6-brands，fa6-regular，fa6-solid，material-symbols，simple-icons
@@ -15,29 +15,34 @@ export const profileConfig: ProfileConfig = {
 	// `pnpm add @iconify-json/<icon-set-name>`
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
+		// {
+		// 	name: "Bilibli",
+		// 	icon: "fa6-brands:bilibili",
+		// 	url: "https://space.bilibili.com/38932988",
+		// 	showName: false,
+		// },
 		{
-			name: "Bilibli",
-			icon: "fa6-brands:bilibili",
-			url: "https://space.bilibili.com/38932988",
+			name: "Homepage",
+			icon: "fa6-solid:globe",
+			url: "https://dztang.net/",
+		},
+		{
+			name: "Email",
+			icon: "fa6-solid:envelope",
+			url: "mailto:tdz@s.hrbcu.edu.cn",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/DaozeTang",
 			showName: false,
 		},
-		{
-			name: "Email",
-			icon: "fa6-solid:envelope",
-			url: "mailto:xiaye@msn.com",
-			showName: false,
-		},
-		{
-			name: "RSS",
-			icon: "fa6-solid:rss",
-			url: "/rss/",
-			showName: false,
-		},
+		// {
+		// 	name: "RSS",
+		// 	icon: "fa6-solid:rss",
+		// 	url: "/rss/",
+		// 	showName: false,
+		// },
 	],
 };
